6ed929cda27e8e2616338161a8d60d1d1f996fa0
tic tac toe <shakryhsyn1@gmail.com>
2020-06-03 18:21:35 +0430

    ..

--- src/employeeTypes/NetworkSecurityExpert.java
+++ src/employeeTypes/NetworkSecurityExpert.java
@@ -1,6 +1,8 @@
 package employeeTypes;
 
-public class NetworkSecurityExpert {
+import extras.CalculationSalary;
+
+public class NetworkSecurityExpert implements CalculationSalary {
     public boolean UTM ;
     public boolean ISMS ;
     public boolean PLC ;
@@ -27,5 +29,60 @@ public class NetworkSecurityExpert {
 
     public void setPLC(boolean PLC) {
         this.PLC = PLC;
+    }
+
+    @Override
+    public double calculateMoneyForHolidayWorks(int days) {
+        return 0;
+    }
+
+    @Override
+    public double calculateMoneyForOverTimeWork(double hours) {
+        return 0;
+    }
+
+    @Override
+    public double calculateMoneyForNightWork(int days) {
+        return 0;
+    }
+
+    @Override
+    public double calculateMoneyForMorningWork(int days) {
+        return 0;
+    }
+
+    @Override
+    public double calculateMoneyForAfternoonWork(int days) {
+        return 0;
+    }
+
+    @Override
+    public double calculateMoneyForFullTimeWork(int days) {
+        return 0;
+    }
+
+    @Override
+    public double calculateMoneyForVacationHour(double hours) {
+        return 0;
+    }
+
+    @Override
+    public double calculateMoneyForTax(double salary) {
+        return 0;
+    }
+
+    @Override
+    public double calculateInsurance(double insurance) {
+        return 0;
+    }
+
+    @Override
+    public double calculateRightToHousing(double baseSalary) {
+        return 0;
+    }
+
+    @Override
+    public double calculateFinalSalary() {
+        return 0;
     }
 }
