f2ee92b1c2c81c519221963a5518e0268c095b6a
aliIsazadeh <isazadhali@gmail.com>
2020-06-03 14:38:58 +0430

    full stack  Employee

--- src/employeeTypes/FullStack.java
+++ src/employeeTypes/FullStack.java
@@ -1,4 +1,157 @@
 package employeeTypes;
 
 public class FullStack {
+    public boolean HTML ;
+    public boolean CSS ;
+    public boolean JC ;
+    public boolean reactJs ;
+    public boolean jQuery ;
+    public boolean mobilePrograming ;
+    public boolean python ;
+    public boolean ruby ;
+    public boolean nodeJs ;
+    public boolean PHP7 ;
+    public boolean ASPNET ;
+    public boolean webServer ;
+    public boolean  SQLServer ;
+    public boolean oracleDatabase ;
+    public boolean xamarin ;
+    public boolean security ;
+    public boolean docker ;
+
+    public boolean isHTML() {
+        return HTML;
+    }
+
+    public void setHTML(boolean HTML) {
+        this.HTML = HTML;
+    }
+
+    public boolean isCSS() {
+        return CSS;
+    }
+
+    public void setCSS(boolean CSS) {
+        this.CSS = CSS;
+    }
+
+    public boolean isJC() {
+        return JC;
+    }
+
+    public void setJC(boolean JC) {
+        this.JC = JC;
+    }
+
+    public boolean isReactJs() {
+        return reactJs;
+    }
+
+    public void setReactJs(boolean reactJs) {
+        this.reactJs = reactJs;
+    }
+
+    public boolean isjQuery() {
+        return jQuery;
+    }
+
+    public void setjQuery(boolean jQuery) {
+        this.jQuery = jQuery;
+    }
+
+    public boolean isMobilePrograming() {
+        return mobilePrograming;
+    }
+
+    public void setMobilePrograming(boolean mobilePrograming) {
+        this.mobilePrograming = mobilePrograming;
+    }
+
+    public boolean isPython() {
+        return python;
+    }
+
+    public void setPython(boolean python) {
+        this.python = python;
+    }
+
+    public boolean isRuby() {
+        return ruby;
+    }
+
+    public void setRuby(boolean ruby) {
+        this.ruby = ruby;
+    }
+
+    public boolean isNodeJs() {
+        return nodeJs;
+    }
+
+    public void setNodeJs(boolean nodeJs) {
+        this.nodeJs = nodeJs;
+    }
+
+    public boolean isPHP7() {
+        return PHP7;
+    }
+
+    public void setPHP7(boolean PHP7) {
+        this.PHP7 = PHP7;
+    }
+
+    public boolean isASPNET() {
+        return ASPNET;
+    }
+
+    public void setASPNET(boolean ASPNET) {
+        this.ASPNET = ASPNET;
+    }
+
+    public boolean isWebServer() {
+        return webServer;
+    }
+
+    public void setWebServer(boolean webServer) {
+        this.webServer = webServer;
+    }
+
+    public boolean isSQLServer() {
+        return SQLServer;
+    }
+
+    public void setSQLServer(boolean SQLServer) {
+        this.SQLServer = SQLServer;
+    }
+
+    public boolean isOracleDatabase() {
+        return oracleDatabase;
+    }
+
+    public void setOracleDatabase(boolean oracleDatabase) {
+        this.oracleDatabase = oracleDatabase;
+    }
+
+    public boolean isXamarin() {
+        return xamarin;
+    }
+
+    public void setXamarin(boolean xamarin) {
+        this.xamarin = xamarin;
+    }
+
+    public boolean isSecurity() {
+        return security;
+    }
+
+    public void setSecurity(boolean security) {
+        this.security = security;
+    }
+
+    public boolean isDocker() {
+        return docker;
+    }
+
+    public void setDocker(boolean docker) {
+        this.docker = docker;
+    }
 }
