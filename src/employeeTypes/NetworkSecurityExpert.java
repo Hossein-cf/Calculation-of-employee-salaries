82c370034e75570b19ca78913c2f9e2481b3dfb5
aliIsazadeh <isazadhali@gmail.com>
2020-06-03 16:04:06 +0430

    security and DBExpert  Employee

--- src/employeeTypes/NetworkSecurityExpert.java
+++ src/employeeTypes/NetworkSecurityExpert.java
@@ -1,4 +1,31 @@
 package employeeTypes;
 
 public class NetworkSecurityExpert {
+    public boolean UTM ;
+    public boolean ISMS ;
+    public boolean PLC ;
+
+    public boolean isUTM() {
+        return UTM;
+    }
+
+    public void setUTM(boolean UTM) {
+        this.UTM = UTM;
+    }
+
+    public boolean isISMS() {
+        return ISMS;
+    }
+
+    public void setISMS(boolean ISMS) {
+        this.ISMS = ISMS;
+    }
+
+    public boolean isPLC() {
+        return PLC;
+    }
+
+    public void setPLC(boolean PLC) {
+        this.PLC = PLC;
+    }
 }
